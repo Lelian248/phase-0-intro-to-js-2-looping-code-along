72db5c03cbbb8f409a38eb4ca341112dc06b3539
lilian manaa <lelian.ibraheem@gmail.com>
2022-09-19 16:35:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+ 
+function writeCards(array, event){
+    const newArray=[]
+    for(let i=0;i<array.length;i++){
+        const mess = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        newArray.push(mess); 
+    }
+    return newArray;
+} 
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+let countDown = (number)=>{ 
+    while(number >=0){
+        console.log(number);
+        number--;
+    }    
+}
+
+countDown(10);
